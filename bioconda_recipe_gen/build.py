e0fedddbb221827730fe38160835b076134e3110
Per Høgfeldt <hogfeldt38@gmail.com>
2019-03-26 11:26:31 +0100

    build.py now have a function for downloading and unpacking sources. This function should probably be moved to some kind of utils.py

--- bioconda_recipe_gen/build.py
+++ bioconda_recipe_gen/build.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import tempfile
+import urllib.request 
 
 DOCKERFILE_TEMPLATE = """
 FROM alpine:3.7
@@ -43,14 +44,9 @@ def bioconda_utils_build(package_name):
 def alpine_docker_build():
     """ Run docker build, to make sure the running docker installation has the requires and up to date image """
     with tempfile.TemporaryDirectory() as tmpdir:
-        with open('%s/Dockerfile' % tmpdir, 'w') as fp:
-            fp.write(DOCKERFILE_TEMPLATE)
-        cmd = [
-            "docker",
-            "build",
-            "--tag=alpine-buildenv",
-            tmpdir,
-        ]
+        with open("%s/Dockerfile" % tmpdir, "w") as fp:
+            write(DOCKERFILE_TEMPLATE)
+        cmd = ["docker", "build", "--tag=alpine-buildenv", tmpdir]
         proc = subprocess.run(cmd, encoding="utf-8", stdout=subprocess.PIPE)
     if proc.returncode != 0:
         return False
@@ -58,18 +54,31 @@ def alpine_docker_build():
         return True
 
 
-def alpine_build():
+def download_and_unpack_source(src, dir_path):
+    if src.split('.')[-2] == 'tar' and src.split('.')[-1] == 'gz':
+        # TODO: Handle exceptions
+        urllib.request.urlretrieve(src, '%s/source.tar.gz' % dir_path)
+        os.mkdir('%s/source' % dir_path)
+        cmd = ['tar', '-xzf', '%s/source.tar.gz' % dir_path, '-C', '%s/source' % dir_path, '--strip-components=1']
+        subprocess.run(cmd, encoding='utf-8', stdout=subprocess.PIPE)
+
+
+def alpine_build(src):
     """ Build a bioconda package with an Alpine Docker image and return the standard output 
     
     Args:
-
+        src: A link to where the source file can be downloaded
     """
     # build alpine image
     if alpine_docker_build():
+        with tempfile.TemporaryDirectory() as tmpdir:
+            download_and_unpack_source(src, tmpdir)
+
         # on succesfull build: Download the source and unpack
-    # Run the alpine image with acces to the source files
-    # make build directory
-    # run $cmake ..
-    # if succesfull
-    # run $make .
-    pass
+        # Run the alpine image with acces to the source files
+        # make build directory
+        # run $cmake ..
+        # if succesfull
+        # run $make .
+    else:
+        pass
