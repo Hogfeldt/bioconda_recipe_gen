5a0cbe63c75d82f509dfd58059b1ff60e844b8c3
Jesper Brink <jesper@brinkandersen.dk>
2019-04-02 15:11:35 +0200

    Impemented the map_alpine_to_conda_pkg method. In order to make this work, there is now a file called alpine_to_conda_map.txt, which contains some of the known packages in alpine and there corresponding conda packages. There are also added test to the method

---
+++ test/test_utils.py
@@ -0,0 +1,16 @@
+import unittest
+from bioconda_recipe_gen.utils import map_alpine_pkg_to_conda_pkg
+
+
+class TestRecipeClass(unittest.TestCase):
+    def test_map_alpine_pkg_to_conda_pkg_hdf5(self):
+        conda_pkg = map_alpine_pkg_to_conda_pkg("hdf5")
+        self.assertEqual("hdf5", conda_pkg)
+        
+    def test_map_alpine_pkg_to_conda_pkg_hdf5(self):
+        conda_pkg = map_alpine_pkg_to_conda_pkg("zlib-dev")
+        self.assertEqual("zlib", conda_pkg)
+
+    def test_map_alpine_pkg_to_conda_pkg_negative(self):
+        conda_pkg = map_alpine_pkg_to_conda_pkg("NotAPackage")
+        self.assertIsNone(conda_pkg)
