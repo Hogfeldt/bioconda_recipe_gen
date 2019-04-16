58bcee11d910b3678285fea391e2ffe2e9b201f9
Per Høgfeldt <hogfeldt38@gmail.com>
2019-04-16 12:45:21 +0200

    Bailout fast if bioconda-recipes path is wrong resolves #28

--- src/bioconda_recipe_gen/cli.py
+++ src/bioconda_recipe_gen/cli.py
@@ -1,10 +1,27 @@
 import argparse
+import os
+import sys
 
 from .bioconda_recipe_gen import main
 
+
+def bioconda_recipes_exists(path):
+    return os.path.exists("%s/recipes" % path) and os.path.exists(
+        "%s/config.yml" % path
+    )
+
+
 def start():
-    parser = argparse.ArgumentParser(description='bioconda-recipe-gen is a tool for automatically generating a bioconda recipe for a given pice of software')
-    parser.add_argument('bioconda_recipe_path', help='Path to your local copy of the bioconda-recipe repository')
+    parser = argparse.ArgumentParser(
+        description="bioconda-recipe-gen is a tool for automatically generating a bioconda recipe for a given pice of software"
+    )
+    parser.add_argument(
+        "bioconda_recipe_path",
+        help="Path to your local copy of the bioconda-recipe repository",
+    )
     args = parser.parse_args()
 
-    main(args.bioconda_recipe_path)
+    if bioconda_recipes_exists(args.bioconda_recipe_path):
+        main(args.bioconda_recipe_path)
+    else:
+        sys.exit("ERROR: Wrong path to bioconda-recipes")
