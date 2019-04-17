375144c3952a0cff61fff1330428b37b81a8cb28
Per Høgfeldt <hogfeldt38@gmail.com>
2019-04-17 11:16:05 +0200

    Added kallisto build.sh as our current build.sh template

--- src/bioconda_recipe_gen/recipes/build.sh
+++ src/bioconda_recipe_gen/recipes/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-mkdir build
-cd build 
-cmake ..
+cd ext/htslib
+autoreconf
+cd ../..
+
+
+mkdir -p $PREFIX/bin
+
+mkdir -p build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
 make
+make install
 
