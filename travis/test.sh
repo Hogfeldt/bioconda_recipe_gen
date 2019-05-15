364f6f194995c403f5a4a427e38536797ecdfec6
Per Høgfeldt <hogfeldt38@gmail.com>
2019-05-15 09:15:50 +0200

    Prettyfied Travis test script

--- travis/test.sh
+++ travis/test.sh
@@ -3,18 +3,30 @@
 
 BR_PATH=./bioconda-recipes
 DATA_PATH=./travis/data
+
 ##### functions
 
 build_kallisto()
 {
-    bioconda-recipe-gen $BR_PATH -n kallisto2 -u https://github.com/pachterlab/kallisto/archive/v0.45.0.tar.gz --tests $DATA_PATH/kallisto -s b32c74cdc0349c2fe0847b3464a3698da89212a507332a06291b6fc27b4e2305 -v 0.45.0 --debug --commands "kallisto version"
+    name=kallisto2
+    url=https://github.com/pachterlab/kallisto/archive/v0.45.0.tar.gz
+    sha=b32c74cdc0349c2fe0847b3464a3698da89212a507332a06291b6fc27b4e2305 
+    tests=$DATA_PATH/kallisto 
+    versio=0.45.0
+    commands="kallisto version"
+    bioconda-recipe-gen $BR_PATH -n $name -u $url --tests $tests -s $sha -v $version --debug --commands $commands
 }
 
 build_htstream()
 {
-    bioconda-recipe-gen $BR_PATH -u https://github.com/ibest/HTStream/archive/v1.0.0-release.tar.gz -n htstream2 -s 4cf9ea61be427ea71c346e438ad2871cf0c7671948819647ef8f182d54e955fe -v 1.0.1  --test-commands "hts_Stats --help" "hts_AdapterTrimmer --help" "hts_CutTrim --help" "hts_NTrimmer --help" "hts_Overlapper --help" "hts_PolyATTrim --help" "hts_QWindowTrim --help" "hts_SeqScreener --help" "hts_SuperDeduper --help" --patches $DATA_PATH/htstream
+    name=htstream2 
+    url=https://github.com/ibest/HTStream/archive/v1.0.0-release.tar.gz 
+    sha=4cf9ea61be427ea71c346e438ad2871cf0c7671948819647ef8f182d54e955fe
+    version=1.0.1
+    commands="hts_Stats --help" "hts_AdapterTrimmer --help" "hts_CutTrim --help" "hts_NTrimmer --help" "hts_Overlapper --help" "hts_PolyATTrim --help" "hts_QWindowTrim --help" "hts_SeqScreener --help" "hts_SuperDeduper --help" 
+    patches=$DATA_PATH/htstream
+    bioconda-recipe-gen $BR_PATH -n $name -u $url -s $sha -v $version --test-commands $commands --patches $patches
 }
-
 
 ##### Choose package to build
 
