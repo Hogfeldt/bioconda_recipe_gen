d14ff61b88ce550400870176aea95e8837e790ea
Jesper Brink <jesper@brinkandersen.dk>
2019-05-23 13:45:04 +0200

    tn93 passes the sanity check.
    We didn't have to do anything (except adding it to the travis file),
    since having just the cxx compiler also accounts for the c compiler.

--- travis/test.sh
+++ travis/test.sh
@@ -10,8 +10,8 @@ build_kallisto()
 {
     name=kallisto2
     url=https://github.com/pachterlab/kallisto/archive/v0.45.0.tar.gz
-    sha=b32c74cdc0349c2fe0847b3464a3698da89212a507332a06291b6fc27b4e2305 
-    tests=$DATA_PATH/kallisto 
+    sha=b32c74cdc0349c2fe0847b3464a3698da89212a507332a06291b6fc27b4e2305
+    tests=$DATA_PATH/kallisto
     version=0.45.0
     commands="kallisto version"
     bioconda-recipe-gen $BR_PATH -n $name -u $url --tests $tests -s $sha -v $version --debug --commands $commands
@@ -19,11 +19,11 @@ build_kallisto()
 
 build_htstream()
 {
-    name=htstream2 
-    url=https://github.com/ibest/HTStream/archive/v1.0.0-release.tar.gz 
+    name=htstream2
+    url=https://github.com/ibest/HTStream/archive/v1.0.0-release.tar.gz
     sha=4cf9ea61be427ea71c346e438ad2871cf0c7671948819647ef8f182d54e955fe
     version=1.0.1
-    commands="hts_Stats --help" "hts_AdapterTrimmer --help" "hts_CutTrim --help" "hts_NTrimmer --help" "hts_Overlapper --help" "hts_PolyATTrim --help" "hts_QWindowTrim --help" "hts_SeqScreener --help" "hts_SuperDeduper --help" 
+    commands="hts_Stats --help" "hts_AdapterTrimmer --help" "hts_CutTrim --help" "hts_NTrimmer --help" "hts_Overlapper --help" "hts_PolyATTrim --help" "hts_QWindowTrim --help" "hts_SeqScreener --help" "hts_SuperDeduper --help"
     patches=$DATA_PATH/htstream
     bioconda-recipe-gen $BR_PATH -n $name -u $url -s $sha -v $version --test-commands $commands --patches $patches
 }
@@ -40,12 +40,22 @@ build_qfilt()
 
 build_libdivsufsort()
 {
-    name=libdivsufsort2 
+    name=libdivsufsort2
     url=https://github.com/y-256/libdivsufsort/archive/5f60d6f026c30fb4ac296f696b3c8b0eb71bd428.tar.gz
     md5=209482686af0b9283659637bd6d51297
     version=2.0.2
-    commands="test -e ${PREFIX}/include/divsufsort.h" "test -e ${PREFIX}/include/divsufsort64.h" 
+    commands="test -e ${PREFIX}/include/divsufsort.h" "test -e ${PREFIX}/include/divsufsort64.h"
     bioconda-recipe-gen $BR_PATH -n $name -u $url -m $md5 -v $version --test-commands $commands
+}
+
+build_tn93()
+{
+    name=tn932
+    url=https://github.com/veg/tn93/archive/v1.0.6.tar.gz
+    sha=f7a9d03a6bd36c8507ed7630693e94d8507ed30f51b325df02ab2ea1726326e4
+    version=1.0.6
+    commands="tn93 -h 2>&1 | grep 'usage'"
+    bioconda-recipe-gen $BR_PATH -n $name -u $url -s $sha -v $version --test-commands "$commands"
 }
 
 ##### Choose package to build
@@ -56,5 +66,6 @@ case $package in
     kallisto)           build_kallisto;;
     htstream)           build_htstream;;
     qfilt)              build_qfilt;;
-    libdivsufsort)  build_libdivsufsort;;
+    libdivsufsort)      build_libdivsufsort;;
+    tn93)               build_tn93;;
 esac
