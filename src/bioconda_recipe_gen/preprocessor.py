d4cb96f38e0e9f397ecabb45cb037aa6424429f4
Per Høgfeldt <hogfeldt38@gmail.com>
2019-05-29 14:33:04 +0200

    Implemented checksum calculation
    It is now optional to supply a checksum.
    If no checksum is given, the program will calculate an md5
    checksum for the source file.

--- src/bioconda_recipe_gen/preprocessor.py
+++ src/bioconda_recipe_gen/preprocessor.py
@@ -1,4 +1,5 @@
 from .recipe import Recipe
+from .utils import calculate_md5_checksum
 
 
 def cmake_recipe_factory(name, version):
@@ -9,18 +10,24 @@ def cmake_recipe_factory(name, version):
     return recipe
 
 
+def add_checksum(recipe, args):
+    if args.sha is not None:
+        recipe.add_checksum_sha256(args.sha)
+    elif args.md5 is not None:
+        recipe.add_checksum_md5(args.md5)
+    else:
+        recipe.add_checksum_md5(calculate_md5_checksum(args.url))
+
+
 def preprocess(args):
     recipe = cmake_recipe_factory(args.name, args.version)
     recipe.add_source_url(args.url)
-    if args.sha is not None:
-        recipe.add_checksum_sha256(args.sha)
-    if args.md5 is not None:
-        recipe.add_checksum_md5(args.md5)
+    add_checksum(recipe, args)
     if args.tests is not None:
         recipe.add_tests(args.tests[0])
     if args.commands is not None:
         recipe.add_test_commands(args.commands)
     if args.patches is not None:
         print(args.patches)
-        recipe.add_patches(args.patches) 
+        recipe.add_patches(args.patches)
     return recipe
