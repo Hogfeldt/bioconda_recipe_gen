19869bbd6a9833f310f6a2a36a7be583b105d843
Jesper Brink <jesper@brinkandersen.dk>
2019-11-05 15:17:42 +0100

    Added missing comma in dictionary

--- src/bioconda_recipe_gen/str_to_pkg.py
+++ src/bioconda_recipe_gen/str_to_pkg.py
@@ -21,7 +21,7 @@ str_to_pkg = {
     "['zlib'] not in reqs/run": ("zlib", "run"),
     "fatal error: zlib.h: no such file or directory": ("zlib", "host"),
     "fatal error: boost": ("boost", "host"), #/opt/conda/conda-bld/lorma_1572881017031/work/thirdparty/gatb-core/src/gatb/tools/designpattern/impl/IteratorHelpers.hpp:32:10: fatal error: boost/variant.hpp: No such file or directory
-    "cmake error: cmake was unable to find a build program corresponding to \"unix makefiles\"": ("make", "build")
+    "cmake error: cmake was unable to find a build program corresponding to \"unix makefiles\"": ("make", "build"),
     "could not find gsl (missing: gsl_libraries)": ("gsl", "host"),
     "fatal error: nlopt.h: no such file or directory": ("nlopt", "host"),
     "make: command not found": ("make", "build"),
